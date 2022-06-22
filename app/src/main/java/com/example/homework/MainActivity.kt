bda2712feebefc03d5c613ebd4f385dc27e1323a
NikitaTodorov <nt.2002ek@gmail.com>
2022-06-22 13:44:19 +0500

    Глава 2

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.homework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextImageButton: ImageButton
+    private lateinit var prevImageButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -25,10 +28,12 @@ class MainActivity : AppCompatActivity() {
     private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        nextButton = findViewById(R.id.next_button)
+        nextImageButton = findViewById(R.id.next_button)
+        prevImageButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
         trueButton.setOnClickListener { view: View ->
             Toast.makeText(
@@ -48,7 +53,7 @@ class MainActivity : AppCompatActivity() {
                 .show()
             checkAnswer(false)
         }
-        nextButton.setOnClickListener {
+        nextImageButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             val questionTextResId = questionBank[currentIndex].textResId
             questionTextView.setText(questionTextResId)
@@ -56,7 +61,34 @@ class MainActivity : AppCompatActivity() {
         }
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText((questionTextResId))
-        updateQuestion()}
+        updateQuestion()
+        prevImageButton.setOnClickListener {
+            if (currentIndex == 0)currentIndex =+ questionBank.size
+            currentIndex = (currentIndex - 1) %questionBank.size
+            updateQuestion()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy() called")
+    }
     private fun updateQuestion(){
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
